b3c0edf907af379db81f99997305449ea601a11d
Rachel Kwan <rachel@Maddys-MacBook-Pro-4.local>
2026-02-11 11:42:18 -0800

    Use .cjs extension for CommonJS compatibility

--- api/send-email.cjs
+++ api/send-email.cjs
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // Handle CORS
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
@@ -75,4 +74,4 @@ export default async function handler(req, res) {
       details: error.message
     });
   }
-}
+};
